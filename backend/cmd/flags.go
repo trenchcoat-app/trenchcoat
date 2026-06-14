9b4c84b6ace91a2ec4d19ce9a343ded61cd6f149
riru <brilljulianrina@gmail.com>
2026-06-14 14:55:25 -0700

    added M for shorthand for skip-migrations

--- backend/cmd/flags.go
+++ backend/cmd/flags.go
@@ -8,6 +8,7 @@ var skipMigrations bool
 
 func parseFlags() {
 	flag.BoolVar(&skipMigrations, "skip-migrations", false, "skip database migrations on startup")
+	flag.BoolVar(&skipMigrations, "M", false, "shorthand for -skip-migrations")
 
 	flag.Parse()
 }
