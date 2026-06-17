01cdb8cfec0c0048173295f7013c179929a2a9c4
CJ Rosales <gargoth13@gmail.com>
2026-06-17 15:18:39 +1200

    test(backend): Create test for sample ping endpoint
    Run with `go test ./...`

---
+++ backend/internal/handlers/ping_test.go
@@ -0,0 +1,31 @@
+package handlers_test
+
+import (
+	"divvy/internal/api"
+	"divvy/internal/server"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-openapi/testify/v2/assert"
+	"github.com/go-openapi/testify/v2/require"
+)
+
+func TestPingRoute(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := server.GetRouter()
+
+	httpRecorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/ping", nil)
+	router.ServeHTTP(httpRecorder, req)
+
+	var pingResponse api.PingResponse
+	err := json.Unmarshal(httpRecorder.Body.Bytes(), &pingResponse)
+	require.NoError(t, err)
+
+	assert.Equal(t, 200, httpRecorder.Code)
+	assert.Equal(t, api.PingResponse{Message: "pong"}, pingResponse)
+}
