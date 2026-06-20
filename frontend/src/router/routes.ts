5c61c08170479ee9142dc2a23828ed5c4fa10185
riru <brilljulianrina@gmail.com>
2026-06-20 13:41:42 -0700

    placeholder signup page, added navbar layout and route

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -2,17 +2,29 @@ import { createRoute } from "@tanstack/react-router";
 import { rootRoute } from "@router/layouts/RootLayout";
 import { Home } from "@components/pages/Home";
 import { About } from "@components/pages/About";
+import { Signup } from "@components/pages/Signup";
+import { navbarRoute } from "./layouts/NavbarLayout";
 
 export const indexRoute = createRoute({
-    getParentRoute: () => rootRoute,
+    getParentRoute: () => navbarRoute,
     path: "/",
     component: Home,
 });
 export const aboutRoute = createRoute({
-    getParentRoute: () => rootRoute,
+    getParentRoute: () => navbarRoute,
     path: "/about",
     component: About,
 });
+export const signupRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/signup",
+    component: Signup
+})
 
-const rootRoutes = [indexRoute, aboutRoute]
-export const routeTree = rootRoute.addChildren([...rootRoutes]);
+export const routeTree = rootRoute.addChildren([
+    navbarRoute.addChildren([
+        indexRoute, 
+        aboutRoute
+    ]),
+    signupRoute,
+]);
