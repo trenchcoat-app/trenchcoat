ce8b1b8c16e7eac47797c1a3b1f8defd31733f2d
CJ Rosales <51285956+Gargoth@users.noreply.github.com>
2026-06-20 17:36:59 +1200

    feat(api): Implement OpenAPI schema for auth endpoints (#9)

---
+++ backend/internal/handlers/sign-up.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"divvy/internal/api"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) SignUp(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, api.ErrorResponse{
+		Code:    "NOT_YET_IMPLEMENTED",
+		Message: "This endpoint is not yet implemented.",
+	})
+}
