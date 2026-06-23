3abf11ed3897902d3633f068f5d3155dcb38c896
riru <brilljulianrina@gmail.com>
2026-06-23 16:39:23 -0700

    added button styling

--- frontend/src/components/shared/Button.tsx
+++ frontend/src/components/shared/Button.tsx
@@ -1,7 +1,12 @@
 import type { ComponentProps } from "react";
+import "./Button.css";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
 export const Button = ({ ref, ...props }: ButtonProps) => {
-    return <button ref={ref} {...props} />;
+    return (
+        <div className="button-wrapper">
+            <button className="button" ref={ref} {...props} />
+        </div>
+    );
 };
