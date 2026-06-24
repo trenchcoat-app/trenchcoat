e995f8c799a07eb625a2ad5c5f832aa4dc53f7a7
riru <brilljulianrina@gmail.com>
2026-06-24 00:23:38 -0700

    changed ValidatorContext type of value from string to any

--- frontend/src/utils/validators/requiredFieldValidator.ts
+++ frontend/src/utils/validators/requiredFieldValidator.ts
@@ -1,6 +1,6 @@
 import type { Validator } from "@/types/validator";
 
-export const requiredFieldValidator: Validator = ({ value }: { value: string }) => {
+export const requiredFieldValidator: Validator = ({ value }: { value: any }) => {
     if (value === undefined || value === null || value === "") return "This field is required";
     return undefined;
 };
