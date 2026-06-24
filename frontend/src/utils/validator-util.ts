ac2b3ccece19bece452eae5000cf95c7129edb8b
riru <brilljulianrina@gmail.com>
2026-06-23 17:51:26 -0700

    added composeValidators comment

--- frontend/src/utils/validator-util.ts
+++ frontend/src/utils/validator-util.ts
@@ -1,5 +1,15 @@
 import type { Validator, ValidatorContext } from "@/types/validator";
 
+/**
+ * Composes multiple validators into a single validator function.
+ *
+ * Each validator is executed with the same validation context, and any returned
+ * error messages (truthy values) are collected into an array.
+ *
+ * @param validators - A list of validator functions to run in sequence.
+ * @returns A single validator function that returns an array of error messages.
+ *          If no validators return errors, the array will be empty.
+ */
 export const composeValidators =
     (...validators: Validator[]) =>
     (context: ValidatorContext) => {
