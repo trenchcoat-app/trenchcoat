27fcb17393fe8ed2d56a7bf2e7e7c7236eb2f023
riru <brilljulianrina@gmail.com>
2026-06-24 15:43:53 -0700

    Improved root, page, and form styling

--- frontend/src/router/layouts/RootLayout.tsx
+++ frontend/src/router/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 const RootLayout = () => {
     return (
-        <main>
+        <main style={{ display: "flex", flexDirection: "column", height: "100%" }}>
             <Outlet />
         </main>
     );
