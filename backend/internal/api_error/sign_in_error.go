7f595a12308e1e2a1f1f0d51af916358a5fdfa67
CJ Rosales <gargoth13@gmail.com>
2026-06-25 22:34:41 +1200

    fix(backend): Return 401 for invalid credential errors

--- backend/internal/api_error/sign_in_error.go
+++ backend/internal/api_error/sign_in_error.go
@@ -4,5 +4,5 @@ import "net/http"
 
 func SignInInvalidCredentialsError() *ApiError {
 	// Keep message intentionally vague for security
-	return &ApiError{Status: http.StatusInternalServerError, Message: "Invalid email or password.", Code: "INTERNAL_SERVER_ERROR"}
+	return &ApiError{Status: http.StatusUnauthorized, Message: "Invalid email or password.", Code: "INTERNAL_SERVER_ERROR"}
 }
