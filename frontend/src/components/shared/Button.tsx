ddbeb1f99ea5a6b637bba5100dd857181b218c75
riru <brilljulianrina@gmail.com>
2026-06-25 11:54:15 -0700

    improved accessibility

--- frontend/src/components/shared/Button.tsx
+++ frontend/src/components/shared/Button.tsx
@@ -1,12 +1,26 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps, MouseEvent } from "react";
 import styles from "./Button.module.css";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export const Button = ({ ref, ...props }: ButtonProps) => {
+export const Button = ({ ref, onClick, ...props }: ButtonProps) => {
+
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+        if (props["aria-disabled"]) {
+            e.preventDefault();
+            return;
+        }
+        onClick?.(e);
+    };
+
     return (
         <div className={styles.buttonWrapper}>
-            <button className={styles.button} ref={ref} {...props} />
+            <button 
+                className={styles.button} 
+                ref={ref} 
+                onClick={handleClick} 
+                {...props} 
+            />
         </div>
     );
 };
