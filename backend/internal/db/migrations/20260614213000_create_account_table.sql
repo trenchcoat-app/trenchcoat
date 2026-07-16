724b55431a7d2487e2699f32c1dff1373611e9bb
riru <brilljulianrina@gmail.com>
2026-07-16 08:03:58 -0700

    tentative: added account_auth table and refactored account table

--- backend/internal/db/migrations/20260614213000_create_account_table.sql
+++ backend/internal/db/migrations/20260614213000_create_account_table.sql
@@ -1,16 +1,33 @@
 -- +goose Up
 CREATE TYPE account_status AS ENUM('active', 'disabled');
+CREATE TYPE account_auth_provider AS ENUM('email'); -- extendable to add providers like 'google', 'apple'
 
 CREATE TABLE IF NOT EXISTS account (
   id UUID PRIMARY KEY DEFAULT uuidv7(),
   email TEXT NOT NULL UNIQUE,
-  username TEXT NOT NULL UNIQUE,
-  password_hash TEXT NOT NULL,
+  username TEXT UNIQUE,
+  verified BOOLEAN NOT NULL DEFAULT false,
   status account_status NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS account_auth (
+  id UUID PRIMARY KEY DEFAULT uuidv7(),
+  account_id UUID UNIQUE REFERENCES account (id) ON DELETE CASCADE,
+  provider account_auth_provider NOT NULL,
+  provider_user_id TEXT, -- null for email account_auth_provider
+  password_hash TEXT, -- non-null for email account_auth_provider
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT chk_provider_fields CHECK (
+    (provider = 'email' AND password_hash IS NOT NULL AND provider_user_id IS NULL) OR
+    (provider != 'email' AND password_hash IS NULL AND provider_user_id IS NOT NULL)
+  )
+);
+
 -- +goose Down
+DROP TABLE IF EXISTS account_auth;
+DROP TYPE IF EXISTS account_auth_provider;
 DROP TABLE IF EXISTS account;
 DROP TYPE IF EXISTS account_status;
