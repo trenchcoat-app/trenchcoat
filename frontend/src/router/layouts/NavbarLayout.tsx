77b7cffea3b6743937385ac0a4204087581f3b6e
riru <brilljulianrina@gmail.com>
2026-08-06 07:30:14 -0700

    moved navbar to its own feature folder

--- frontend/src/router/layouts/NavbarLayout.tsx
+++ frontend/src/router/layouts/NavbarLayout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/components/features/Navbar";
+import { Navbar } from "@/components/features/navbar/Navbar";
 import { Outlet } from "@tanstack/react-router";
 
 export const NavbarLayout = () => {
